a2f94467cb0a3bfeab5a9b1a83bc53bb01580ea0
DESKTOP-AIB0SKK\Juan Luis <jlljunior_90@hotmail.com>
2017-08-02 20:14:57 +0200

    Ahora se devuelve toda una estructura

--- src/Preprocesador/Recolector.py
+++ src/Preprocesador/Recolector.py
@@ -1,35 +1,34 @@
 import os
 import sys
-#from ...definitions import ROOT_DIR
 
-class Recolector:
-    #ROOT_DIR=os.path.dirname(os.path.abspath(__file__))
-    script_path = os.path.abspath(__file__) # i.e. /path/to/dir/foobar.py
-    script_dir = os.path.split(script_path)[0] #i.e. /path/to/dir/
-    script_dir = script_dir.replace("\src\Preprocesador","")
-    textos = []
-    @staticmethod
-    def readFile(path):
-        thePath = os.path.join(Recolector.script_dir, path)
-        file=open(thePath,"r")
-        lines = file.read().lower()
-        
-        
-        return lines 
-    @staticmethod
-    def readAllFiles(path):
-      
-        basePath = path
-        names = os.listdir(path)
-       
-        for directorio in names:
-            newPath = os.path.join(basePath,directorio)
-            if os.path.isfile(newPath):
-                Recolector.textos.append(Recolector.readFile(newPath))
-            else:
-                Recolector.readAllFiles(newPath)    
+script_path = os.path.abspath(__file__) # i.e. /path/to/dir/foobar.py
+script_dir = os.path.split(script_path)[0] #i.e. /path/to/dir/
+script_dir = script_dir.replace("\src\Preprocesador","")
+textos = dict()
 
-        return names
+def readFile(path):
+    thePath = os.path.join(script_dir, path)
+    file=open(thePath,"r")
+    lines = file.read().lower()
+    
+    
+    return lines 
 
-print(Recolector.readAllFiles("resources"))
-print(Recolector.textos)
+def readAllFiles(path):
+    
+    basePath = path
+    names = os.listdir(path)
+    
+    for directorio in names:
+        newPath = os.path.join(basePath,directorio)
+        if os.path.isfile(newPath):
+            array = textos.get(newPath,[])
+            array.append(readFile(newPath))
+            textos[newPath.replace(directorio,"")] = array
+        else:
+            readAllFiles(newPath)    
+
+    return names
+
+print(readAllFiles("resources"))
+print(textos)
