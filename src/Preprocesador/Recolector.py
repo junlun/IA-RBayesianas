758b03afe403681be496ce6315086527d92d632f
DESKTOP-AIB0SKK\Juan Luis <jlljunior_90@hotmail.com>
2017-08-02 19:38:16 +0200

    ahora se lee dentro de una carpeta entera

--- src/Preprocesador/Recolector.py
+++ src/Preprocesador/Recolector.py
@@ -7,7 +7,7 @@ class Recolector:
     script_path = os.path.abspath(__file__) # i.e. /path/to/dir/foobar.py
     script_dir = os.path.split(script_path)[0] #i.e. /path/to/dir/
     script_dir = script_dir.replace("\src\Preprocesador","")
-    
+    textos = []
     @staticmethod
     def readFile(path):
         thePath = os.path.join(Recolector.script_dir, path)
@@ -18,8 +18,18 @@ class Recolector:
         return lines 
     @staticmethod
     def readAllFiles(path):
+      
         basePath = path
+        names = os.listdir(path)
+       
+        for directorio in names:
+            newPath = os.path.join(basePath,directorio)
+            if os.path.isfile(newPath):
+                Recolector.textos.append(Recolector.readFile(newPath))
+            else:
+                Recolector.readAllFiles(newPath)    
 
-        return True
+        return names
 
-print(Recolector.readFile("resources\prueba.txt"))
+print(Recolector.readAllFiles("resources"))
+print(Recolector.textos)
